56334f92a2d83fb32e347577af3982ccfcf055bc
nilsgsn <nils.uebach@gesamtschulenord.nrw.schule>
2025-02-20 23:38:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ st.markdown("<br><br><br>", unsafe_allow_html=True)  # Fügt Platz ein
 st.markdown("<p style='text-align: center; font-size: smaller;'>created by Mr Übach in collaboration with ChatGPT</p>", unsafe_allow_html=True)
 
 # Schul-Logo hinzufügen
-st.image("school_logo.png", width=100, use_container_width=True)
+st.image("school_logo.png", width=50, use_container_width=True)
