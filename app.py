4452ebf1bf9b9bb0e8328aed4bb86d67e3e8c22b
nilsgsn <nils.uebach@gesamtschulenord.nrw.schule>
2025-02-21 17:27:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -96,5 +96,9 @@ if st.button("Generate Warm-Up", key="generate_button"):
 st.markdown("<hr>", unsafe_allow_html=True)  # Trennlinie für besseren Look
 st.markdown("<br><br><br>", unsafe_allow_html=True)  # Fügt Platz ein
 st.markdown("<p style='text-align: center; font-size: smaller;'>made by Nils Übach in collaboration with ChatGPT</p>", unsafe_allow_html=True)
-
+import streamlit as st
+# URL des Logos
+logo_url = "https://raw.githubusercontent.com/nilsgsn/mathespiele/main/school_logo.png"
+# Logo anzeigen
+st.image(logo_url, width=150)
 
