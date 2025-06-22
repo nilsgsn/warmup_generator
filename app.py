1c2be872e6bddd33445dccf7b46f1fcd3809d75b
nilsgsn <nils.uebach@gesamtschulenord.nrw.schule>
2025-06-22 16:55:34 +0200

    Remove redundant import and use local logo

--- app.py
+++ app.py
@@ -96,9 +96,6 @@ if st.button("Generate Warm-Up", key="generate_button"):
 st.markdown("<hr>", unsafe_allow_html=True)  # Trennlinie für besseren Look
 
 st.markdown("<p style='text-align: center; font-size: smaller;'>made by Nils Übach in collaboration with ChatGPT</p>", unsafe_allow_html=True)
-import streamlit as st
-# URL des Logos
-logo_url = "https://raw.githubusercontent.com/nilsgsn/mathespiele/main/school_logo.png"
 # Logo anzeigen
-st.image(logo_url, width=150)
+st.image("school_logo.png", width=150)
 
